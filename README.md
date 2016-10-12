a7bbcd7bfe9fcfed14e73736af86c58c2497d128
Aleksandr Sidorov <shtil80@gmail.com>
2016-10-12 17:13:27 +0700

    Initial commit.
    Added file structure. Ctreated SearchBar component.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Youtube Viever
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import SearchBar from './components/SearchBar';
+
+//const API_KEY = 'AIzaSyDNBG9sECeg6hdzyhZ9XId8KEcoAUaezd0';
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
